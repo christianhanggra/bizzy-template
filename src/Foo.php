7a88214798b472504bed6bf53d2275c72ab0c0fa
Christian Hanggra <christianhanggra@gmail.com>
2017-05-12 14:05:22 +0700

    Penambahan function

--- src/Foo.php
+++ src/Foo.php
@@ -4,11 +4,33 @@ namespace Christianhanggra\Bizzy\Template;
 
 use PDF;
 use GuzzleHttp\Client;
+use Christianhanggra\Bizzy\Template\Model\User;
 
 class Foo
 {
-	public function bar($str)
+	public function bar($arr)
 	{
-		return $str;
+		return $arr;
 	}
+
+	public function users($limit=10)
+	{
+		return User::take($limit)->get();
+	}
+
+	public function getcurl($url)
+	{	
+		$client = new Client();
+		try {
+            $content = $client->get($url);
+            if (empty($content)) return ['status' => false];
+            $arr = json_decode(@$content->getBody(), true);
+            return $arr;
+
+        } catch (\Exception $e) {
+
+            echo 'Caught exception: ', $e->getMessage(), "\n";
+        }
+	}
+
 }
---
+++ src/Model/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Christianhanggra\Bizzy\Template\Model;
+
+use Moloquent\Eloquent\Model;
+
+class User extends Model
+{
+    protected $connection = "mongodb";
+}
